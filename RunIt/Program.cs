f488d1c82cd8ee70635a744c851353c78f87327c
Dmitry Tarasov <dmitry.tarasov@live.com>
2018-12-19 20:04:24 +0500

    Add project files.

---
+++ RunIt/Program.cs
@@ -0,0 +1,40 @@
+﻿
+namespace RunIt
+{
+    using System.Configuration;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Threading;
+
+    static class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                try
+                {
+                    for (int i = 0; i < ConfigurationManager.AppSettings.Count; i++)
+                    {
+                        string filePath = ConfigurationManager.AppSettings[i];
+                        string fullFileName = Path.GetFileName(filePath);
+                        string fileName = Path.GetFileNameWithoutExtension(fullFileName);
+                        var proc = Process.GetProcessesByName(fileName);
+                        if (proc == null || proc.Length == 0)
+                        {
+                            ProcessStartInfo startInfo = new ProcessStartInfo(filePath);
+                            startInfo.WorkingDirectory = Path.GetDirectoryName(filePath);
+                            Process.Start(startInfo);
+                            Thread.Sleep(1000);
+                        }
+                    }       
+                }
+                catch
+                { 
+                }
+
+                Thread.Sleep(10000);
+            }
+        }
+    }
+}
---
+++ RunIt/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RunIt")]
+[assembly: AssemblyDescription("RunIt")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RunIt")]
+[assembly: AssemblyCopyright("Copyright © Dmitry Tarasov, 2018")]
+[assembly: AssemblyTrademark("RunIt")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.18.12.12")]
+[assembly: AssemblyFileVersion("1.18.12.12")]
